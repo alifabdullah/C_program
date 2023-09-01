7675fabbdc54d60d3b3629ab0e6099f5061aaf18
Alif Abdullah <78503032+alifabdullah@users.noreply.github.com>
2023-09-01 15:33:34 +0600

    Add files via upload

---
+++ function.c
@@ -0,0 +1,99 @@
+/*#include<stdio.h>
+// diclaration/prototype
+void printhello();
+void printgoodbye();
+void hindu();
+void muslim();
+
+
+int main(){
+
+   /* printhello();//function call
+    printhello();
+    printhello();
+   
+    printgoodbye();
+    printgoodbye();
+   
+    printhello();
+    printhello();
+   
+    printf("enter H for Hindu And M for Muslim:");
+    char ch;
+    scanf("%c",&ch);
+    if (ch == 'h')
+    {
+        hindu();
+    }else{ muslim();}
+    
+return 0;
+}
+void hindu(){
+    printf("nomoskar\n");
+}
+void muslim(){
+    printf("salam walaykum");
+}
+//function defination
+void printhello(){
+    printf("hello!\n");
+    printf(" My name is Alif Abdullah\n");
+}
+void printgoodbye(){
+    printf("Good Bye\n");
+}
+#include<stdio.h>
+int sum(int a ,int b);
+int main(){
+    int a,b;
+    printf("Enter First number:");
+    scanf("%d",&a);
+    printf("Enter Second number:");
+    scanf("%d",&b);
+    int s = sum (a,b);
+    printf("sum is: %d\n",s);
+}
+
+int sum(int a, int b)
+{
+    return a+b;
+}*/
+/*#include<stdio.h>
+//int sum(int a ,int b);
+void printable(int n);
+int main(){
+    int n;
+    printf("Enter First number:");
+    scanf("%d",&n);
+    printable(n);//argument /actual parameter
+    return 0;
+}
+
+/*int sum(int a, int b)
+{
+    return a+b;
+}*/
+/*void printable(int n){//parameter/formal perameter
+    
+    for (int i =1; i <=10; i++)
+    {
+       printf("%d\n",i*n); 
+    }
+    
+    
+}
+#include<stdio.h>
+void calculateprice(float value);
+int main(){
+    float value=100;
+    
+   
+    calculateprice(value);
+    return 0;
+}
+
+void calculateprice(float value)
+{ value=value+(0.18*value);
+printf("price is :%f",value);
+}
+*/
